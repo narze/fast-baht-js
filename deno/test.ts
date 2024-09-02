a1c7e56d487e357e6f26713cb60ff38db2aeaa77
Manassarn Manoonchai <manassarn@gmail.com>
2024-09-02 22:52:51 +0700

    Fix deno test

--- deno/test.ts
+++ deno/test.ts
@@ -3,8 +3,8 @@ import { assertEquals } from 'jsr:@std/assert@1';
 import Baht from '../src/index.ts';
 
 function assertBaht(value: number, expected: string) {
-  console.log(`${value} -> ${Baht.convert(value)} (Expected: ${expected})`);
-  assertEquals(Baht.convert(value), expected);
+  console.log(`${value} -> ${Baht(value)} (Expected: ${expected})`);
+  assertEquals(Baht(value), expected);
 }
 
 Deno.test('Baht.js works on Deno', () => {
